9c4574669e705ff03167107f066b74bdd359a31f
guiarese <guilhermearesende1@gmail.com>
2021-12-18 18:14:24 -0300

    first commit

---
+++ src/main/java/br/com/guiareze/atualizapedido/AtualizaPedidoApplication.java
@@ -0,0 +1,13 @@
+package br.com.guiareze.atualizapedido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtualizaPedidoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtualizaPedidoApplication.class, args);
+	}
+
+}
